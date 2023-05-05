e983abbb485b4383c4ec30abfa5e2201b4b0cba4
xypage <xypage@gmail.com>
2023-05-05 15:41:58 -0700

    Profiles show followers, added data loader function

--- app.py
+++ app.py
@@ -5,8 +5,17 @@
 from sys import stdout
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
-from flask import Flask, render_template, request, url_for, redirect, session, jsonify, render_template_string
-from datetime import datetime
+from flask import (
+    Flask,
+    render_template,
+    request,
+    url_for,
+    redirect,
+    session,
+    jsonify,
+    render_template_string,
+)
+from datetime import datetime, timedelta
 
 # Database
 from flask_sqlalchemy import SQLAlchemy
@@ -36,8 +45,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 # Admin
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-admin = Admin(app, name='Admin View', template_mode='bootstrap3')
+app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
+admin = Admin(app, name="Admin View", template_mode="bootstrap3")
 
 
 @login_manager.user_loader
@@ -66,16 +75,17 @@ db = SQLAlchemy(app)
 
 # Association table for who the user is following
 follows = db.Table(
-    'follows',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    "follows",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
 )
 # Association table for posts the user likes
 likes = db.Table(
-    'likes',
-    db.Column('liker_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    "likes",
+    db.Column("liker_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("post_id", db.Integer, db.ForeignKey("post.id"), primary_key=True),
 )
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -88,18 +98,24 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable=False)
     bio = db.Column(db.String)
     # Posts is a list of all the posts the user has made
-    posts = db.relationship('Post', backref='user')
+    posts = db.relationship("Post", backref="user")
     # Follows is a list of all the users the user is following
     followed = db.relationship(
-        'User', secondary=follows,
-        primaryjoin=(follows.c.follower_id==id),
-        secondaryjoin=(follows.c.followed_id==id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+        "User",
+        secondary=follows,
+        primaryjoin=(follows.c.follower_id == id),
+        secondaryjoin=(follows.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic",
+    )
     liked = db.relationship(
-        'Post', secondary=likes,
-        primaryjoin=(likes.c.liker_id==id),
-        secondaryjoin=(likes.c.post_id==id),
-        backref=db.backref('likes', lazy='dynamic'), lazy='dynamic')
+        "Post",
+        secondary=likes,
+        primaryjoin=(likes.c.liker_id == id),
+        secondaryjoin=(likes.c.post_id == id),
+        backref=db.backref("likes", lazy="dynamic"),
+        lazy="dynamic",
+    )
 
     def __init__(self, username, name, password, role):
         self.username = username
@@ -115,24 +131,112 @@ class User(db.Model, UserMixin):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     # user_id is the id of the user who made the post (foreign key)
-    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     # content is the content of the post
-    timestamp = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.String, nullable=False)
     # timestamp is the time the post was made
-    privacy_setting = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
     # privacy_setting is the privacy setting of the post
+    privacy_setting = db.Column(db.String, nullable=False)
+    comments = db.relationship("Comment", back_populates="parent_post")
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    contents = db.Column(db.String)
+    parent_post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    parent_post = db.relationship("Post", back_populates="comments")
+
+
+from random import randint
+
+
+def load_data():
+    # Make a bunch of names/passwords
+    users = [
+        ["Xavier", "xypage", "password"],
+        ["Adam", "adam", "password"],
+        ["Bob", "bob", "password"],
+        ["Charlie", "charlie", "password"],
+        ["Dylan", "dylan", "password"],
+        ["Emily", "emily", "password"],
+        ["Frank", "frank", "password"],
+        ["George", "george", "password"],
+        ["Harry", "harry", "password"],
+        ["Isabelle", "isabelle", "password"],
+        ["John", "john", "password"],
+        ["Katy", "katy", "password"],
+    ]
+    # Load some filler text we can use for the bios
+    with open("lorem.txt", "r") as lorem_file:
+        lorem_text = lorem_file.read().split(".")
+
+    with app.app_context():
+        # Create all the users
+        for i in range(len(users)):
+            new_user = User(users[i][1], users[i][0], users[i][2], "user")
+            # Set their bios to random parts of the filler text
+            lorem_index = randint(0, len(lorem_text) - 5)
+            new_user.bio = ".".join(lorem_text[lorem_index : lorem_index + 5]) + "."
+            db.session.add(new_user)
+            db.session.commit()
+            # Replace the user in the list (just an array) with the user's id
+            users[i] = new_user.id
+
+        # Iterate through all the users, following random other users and making random posts
+        for user_id in users:
+            user = User.query.get(user_id)
+            # Decide how many they should each follow, between 1 and the number of users - 1
+            num_to_follow = randint(1, len(users) - 1)
+            followed = 0
+            while followed < num_to_follow:
+                # Pick a user to follow
+                user_to_follow = User.query.get(users[randint(0, len(users) - 1)])
+                # Only add them if it isn't the current user
+                if user_to_follow.id != user.id and user_to_follow not in user.followed:
+                    user.followed.append(user_to_follow)
+                    db.session.commit()
+                    followed += 1
+
+            for post_num in range(randint(1, 5)):
+                post_length = randint(1, 5)
+                lorem_index = randint(0, len(lorem_text) - post_length)
+                post_text = (
+                    ".".join(lorem_text[lorem_index : lorem_index + post_length]) + "."
+                )
+
+                # Create a new post object
+                # Get the current time and subtract a random amount of time from it,
+                # that way all the posts don't say they were made at the same time
+                post_time = datetime.now() - timedelta(
+                    days=randint(0, 30),
+                    hours=randint(0, 24),
+                    minutes=randint(0, 60),
+                    seconds=randint(0, 60),
+                )
+                new_post = Post(
+                    user_id=user.id,
+                    content=post_text,
+                    timestamp=post_time,
+                    privacy_setting="public",
+                )
+                db.session.add(new_post)
+                db.session.commit()
+
 
 with app.app_context():
-    # db.drop_all()
+    pass
+    db.drop_all()
     db.create_all()
+    load_data()
+
 
 ##################################################
 #         Post Management (User View)            #
 ##################################################
 
-@app.route("/new_post", methods=["GET", "POST"])
+
+@app.route("/new_post", methods=["POST"])
 @login_required
 def new_post():
     # Get the post data from the form
@@ -144,7 +248,7 @@ def new_post():
         user_id=current_user.id,
         content=content,
         timestamp=datetime.now(),
-        privacy_setting="public"
+        privacy_setting="public",
     )
     db.session.add(new_post)
     db.session.commit()
@@ -168,8 +272,9 @@ def new_post():
     # post_html = render_template_string('<div> class="post"><p>{{ content}}</p><p>{{ timestamp }}</p></div>', content=content, timestamp=new_post.timestamp)
     # return jsonify(post_html)
 
+
 # endpoint to get all posts from the database (global feed)
-@app.route('/get_all_posts', methods=["GET"])
+@app.route("/get_all_posts", methods=["GET"])
 @login_required
 def get_all_posts():
     all_posts = Post.query.order_by(Post.timestamp.desc()).all()
@@ -182,24 +287,31 @@ def get_all_posts():
             "timestamp": post.timestamp,
             "username": user.username,
             "name": user.name,
-            "id": post.id
+            "id": post.id,
         }
         posts_data.append(post_data)
 
     return jsonify(posts_data)
 
+
 # endpoint to get the posts of who the user is following (following feed)
-@app.route('/get_following_posts', methods=["GET"])
+@app.route("/get_following_posts", methods=["GET"])
 @login_required
 def get_following_posts():
     # get the unique ids of the people the current user is following
     following_ids = [user.id for user in current_user.followed]
+    # A users own posts should show up in their feed as well
+    following_ids.append(current_user.id)
     # get all the posts form the following ids
-    following_posts = Post.query.filter(Post.user_id.in_(following_ids)).order_by(Post.timestamp.desc()).all()
+    following_posts = (
+        Post.query.filter(Post.user_id.in_(following_ids))
+        .order_by(Post.timestamp.desc())
+        .all()
+    )
     post_list = []
     for post in following_posts:
         user = User.query.filter_by(id=post.user_id).first()
-        post_data ={
+        post_data = {
             "content": post.content,
             "timestamp": post.timestamp,
             "username": user.username,
@@ -211,9 +323,11 @@ def get_following_posts():
 
 @app.route("/user/posts/<string:username>")
 def get_users_posts(username):
-    user =  User.query.filter_by(username=username).first()
+    user = User.query.filter_by(username=username).first()
     user_id = user.id
-    users_posts = Post.query.filter_by(user_id=user_id).order_by(Post.timestamp.desc()).all()
+    users_posts = (
+        Post.query.filter_by(user_id=user_id).order_by(Post.timestamp.desc()).all()
+    )
     post_list = []
     for post in users_posts:
         post_data = {
@@ -225,20 +339,27 @@ def get_users_posts(username):
         post_list.append(post_data)
     return jsonify(post_list)
 
+
 ##################################################
 #         User Info Management (User View)       #
 ##################################################
 
-@app.route('/get_current_user_name', methods=["Get"])
+
+@app.route("/get_current_user_name", methods=["Get"])
 @login_required
 def get_current_user_name():
     print(current_user.name)
     return current_user.name
 
-@app.route('/get_current_user_posts', methods=["GET"])
+
+@app.route("/get_current_user_posts", methods=["GET"])
 @login_required
 def get_current_user_posts():
-    current_user_posts = Post.query.filter_by(user_id=current_user.id).order_by(Post.timestamp.desc()).all()
+    current_user_posts = (
+        Post.query.filter_by(user_id=current_user.id)
+        .order_by(Post.timestamp.desc())
+        .all()
+    )
     post_list = []
     for post in current_user_posts:
         post_data = {
@@ -252,12 +373,14 @@ def get_current_user_posts():
     print(post_list)
     return jsonify(post_list)
 
+
 @app.route("/edit_bio", methods=["POST"])
 @login_required
 def edit_bio():
     current_user.bio = request.get_json()["new_bio"]
     db.session.commit()
     return "Bio successfully updated", 200
+
 
 @app.route("/like/<int:post_id>", methods=["POST"])
 @login_required
@@ -274,18 +397,8 @@ def like_post(post_id):
 ##################################################
 #         Follow Management (User View)          #
 ##################################################
-
-# route to get list of users that the current user is not following
-@app.route('/get_unfollowed_users', methods=["GET"])
-@login_required
-def get_unfollowed_users():
-    followed_user_ids = [user.id for user in current_user.followed]
-    unfollowed_users = User.query.filter(User.id.notin_(followed_user_ids + [current_user.id])).all()
-    user_data = [{"id": user.id, "username": user.username, "name": user.name} for user in unfollowed_users]
-    return jsonify(user_data)
-
 # route for follow request
-@app.route('/follow_user/<int:user_id>', methods=["POST"])
+@app.route("/follow_user/<int:user_id>", methods=["POST"])
 @login_required
 def follow_user(user_id):
     user_to_follow = User.query.get(user_id)
@@ -299,18 +412,31 @@ def follow_user(user_id):
             return "Already following user", 200
     return "User to follow is not found", 404
 
-# route to get list of users that that the current user is following
-@app.route('/get_followed_users', methods=["GET"])
-@login_required
-def get_followed_users():
-    followed_users_id = [user.id for user in current_user.followed]
-    followed_users = User.query.filter(User.id.in_(followed_users_id)).all()
-    user_data = [{"id": user.id, "username": user.username, "name": user.name} for user in followed_users]
-    return jsonify(user_data)
-    
 
-#route for unfollow request
-@app.route('/unfollow_user/<int:user_id>', methods=["POST"])
+# route to get list of users that that the current user is following
+@app.route("/user/<string:username>/following", methods=["GET"])
+@login_required
+def get_followed_users(username):
+    followed_users = User.query.filter_by(username=username).first().followed
+    user_data = [
+        {"id": user.id, "username": user.username, "name": user.name}
+        for user in followed_users
+    ]
+    return jsonify(user_data)
+
+@app.route("/user/<string:username>/followed_by", methods=["GET"])
+@login_required
+def get_followers(username):
+    followers = User.query.filter_by(username=username).first().followers
+    user_data = [
+        {"id": user.id, "username": user.username, "name": user.name}
+        for user in followers
+    ]
+    return jsonify(user_data)
+
+
+# route for unfollow request
+@app.route("/unfollow_user/<int:user_id>", methods=["POST"])
 @login_required
 def unfollow_user(user_id):
     user_to_unfollow = User.query.get(user_id)
@@ -322,6 +448,21 @@ def unfollow_user(user_id):
         db.session.commit()
         return "Successfully unfollowed user", 200
     return "User to unfollow is not found", 404
+
+# route for removing a follower from the current user
+@app.route("/remove_follower/<int:user_id>", methods=["POST"])
+@login_required
+def remove_follower(user_id):
+    user_to_make_unfollow = User.query.get(user_id)
+    # check to make sure user exists
+    if user_to_make_unfollow is not None:
+        # remove the user to be unfollowed from the association table
+        user_to_make_unfollow.followed.remove(current_user)
+        # update database
+        db.session.commit()
+        return "Successfully unfollowed user", 200
+    return "User to unfollow is not found", 404
+
 
 ##################################################
 #                Login Management                #
@@ -356,7 +497,11 @@ def register():
     elif request.method == "POST":
         # Make new user using data in request.form
         new_user = User(
-            request.form["username"], request.form["name"], request.form["password"], "user")
+            request.form["username"],
+            request.form["name"],
+            request.form["password"],
+            "user",
+        )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user, remember=True)
@@ -396,39 +541,49 @@ def about():
 def events():
     return render_template("events.html")
 
+
 @app.route("/profile/")
 @login_required
 def viewProfile():
     # return render_template("profile.html")
     return redirect("/user/" + current_user.username)
 
+
 @app.route("/user/<string:username>")
 @login_required
 def user_profile(username):
     user = User.query.filter_by(username=username).first()
     # get all the posts from this user
-    users_posts = Post.query.filter_by(user_id=user.id).order_by(Post.timestamp.desc()).all()
+    users_posts = (
+        Post.query.filter_by(user_id=user.id).order_by(Post.timestamp.desc()).all()
+    )
     post_list = []
     for post in users_posts:
-        post_data = {
-            "content": post.content,
-            "timestamp": post.timestamp
-        }
+        post_data = {"content": post.content, "timestamp": post.timestamp}
         post_list.append(post_data)
 
     user = {
         "name": user.name,
-        "username": user.username, 
+        "username": user.username,
         "user_id": user.id,
         "followed": current_user.is_authenticated and user in current_user.followed,
         "bio": user.bio,
-        "is_current": current_user.is_authenticated and current_user.username == username
+        "is_current": current_user.is_authenticated
+        and current_user.username == username,
     }
     return render_template("test_profile.html", post_list=post_list, user=user)
 
 
+# @app.route("/user/<string:username>/following")
+# @login_required
+# def user_following(username):
+#     following_user = User.query.filter_by(username=username).first()
+#     followed_users = [
+#         {"name": user.name, "username": user.username, "id": user.id}
+#         for user in following_user.followed
+#     ]
+#     return jsonify(followed_users)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    
